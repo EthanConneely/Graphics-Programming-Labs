f2151c2ef47571f2f507d819a6267178c0aebd31
IrishBruse <Econn50@outlook.com>
2022-12-06 10:15:17 +0000

    Add camera rotation to unity

--- Lab7/Assets/_Scripts/CameraController.cs
+++ Lab7/Assets/_Scripts/CameraController.cs
@@ -5,6 +5,12 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public Vector3 offset = new(0, 10, -12);
 
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
     private void Start()
     {
 
@@ -13,5 +19,10 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         transform.position = target.position + offset;
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
